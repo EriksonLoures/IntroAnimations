0986cfb53c15d87d6387e80d257dabcf0d86ce22
Erikson Loures <erikson992@gmail.com>
2023-05-05 14:49:43 -0300

    Initial Commit

---
+++ PagesAnimations/PagesAnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PagesAnimationsApp.swift
+//  PagesAnimations
+//
+//  Created by Erik Loures on 05/05/23.
+//
+
+import SwiftUI
+
+@main
+struct PagesAnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
