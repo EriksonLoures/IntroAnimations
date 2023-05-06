97094fba4c3387fc1e6534553a7986d4d1959a7b
Erikson Loures <erikson992@gmail.com>
2023-05-05 22:57:24 -0300

    first commit

---
+++ PagesAnimations/Model/OnBoardingItem.swift
@@ -0,0 +1,20 @@
+//
+//  OnBoardingItem.swift
+//  PagesAnimations
+//
+//  Created by Erik Loures on 05/05/23.
+//
+
+import SwiftUI
+
+struct OnBoardingItem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct OnBoardingItem_Previews: PreviewProvider {
+    static var previews: some View {
+        OnBoardingItem()
+    }
+}
---
+++ PagesAnimations/View/OnboardingScreen.swift
@@ -0,0 +1,20 @@
+//
+//  OnboardingScreen.swift
+//  PagesAnimations
+//
+//  Created by Erik Loures on 05/05/23.
+//
+
+import SwiftUI
+
+struct OnboardingScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct OnboardingScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardingScreen()
+    }
+}
